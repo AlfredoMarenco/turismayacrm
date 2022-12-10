a0a5c99853919523b76469c60b889a4cff5e3ced
Alvar <alvarbu@gmail.com>
2022-12-10 13:15:23 -0600

    se corrige boton en panel de unidades

--- resources/views/livewire/units.blade.php
+++ resources/views/livewire/units.blade.php
@@ -6,7 +6,7 @@
                     <div class="md:pl-5 lg:pl-8 min-w-0 flex-1">
                         <h2
                             class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                            Panel de Pagos</h2>
+                            Panel de Unidades</h2>
                         <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
 
                             <div class="mt-2 flex items-center text-sm text-gray-500">
@@ -36,7 +36,7 @@
                                         d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                Crear solicitud de pago
+                                Agregar unidad
                             </button>
                         </span>
                     </div>
