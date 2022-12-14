0766419537ec713930be9237cab45ccf93258699
Alvar <alvarbu@gmail.com>
2022-12-13 18:33:44 -0600

    se corrige titulo de vista detalle cliente

--- resources/views/livewire/detailclient.blade.php
+++ resources/views/livewire/detailclient.blade.php
@@ -9,7 +9,7 @@
                         <div class="md:pl-5 lg:pl-8 min-w-0 flex-1">
                             <h2
                                 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                                Panel de Clientes</h2>
+                                Jane Cooper</h2>
                             <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
 
                                 <div class="mt-2 flex items-center text-sm text-gray-500">
