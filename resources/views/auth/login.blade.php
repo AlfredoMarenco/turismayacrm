90894479432767deda1175a682a7f94f504a723f
unknown <alfredomarenco@boletea.com>
2023-01-13 22:17:54 -0600

    Avances en primera parte de los clientes y del login nos falta crear la funcionalidad del login y la funcionalidades de los clientes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,8 +47,7 @@
     </x-jet-authentication-card> --}}
     <div class="flex h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-blue-900">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-12 w-auto" src="/img/turismaya-logo.svg"
-                alt="Your Company">
+            <img class="mx-auto h-12 w-auto" src="/img/turismaya-logo.svg" alt="Your Company">
             <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-white">Bienvenido a tu portal de pago</h2>
             {{-- <p class="mt-2 text-center text-sm text-gray-600">
                 Inicia sesión
@@ -57,7 +56,15 @@
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form class="space-y-6" action="#" method="POST">
+                <x-jet-validation-errors class="mb-4" />
+
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form class="space-y-6" method="POST" action="{{ route('login') }}">
+                    @csrf
                     <div>
                         <label for="email" class="block text-sm font-medium text-gray-700">Correo</label>
                         <div class="mt-1">
@@ -69,7 +76,8 @@
                     <div>
                         <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
                         <div class="mt-1">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required
+                            <input id="password" name="password" type="password" autocomplete="current-password"
+                                required
                                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                         </div>
                     </div>
@@ -82,13 +90,15 @@
                         </div>
 
                         <div class="text-sm">
-                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">¿Olvidaste tu contraseña?</a>
+                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">¿Olvidaste tu
+                                contraseña?</a>
                         </div>
                     </div>
 
                     <div>
                         <button type="submit"
-                            class="flex w-full justify-center rounded-md border border-transparent bg-blue-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Iniciar sesión</button>
+                            class="flex w-full justify-center rounded-md border border-transparent bg-blue-900 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Iniciar
+                            sesión</button>
                     </div>
                 </form>
             </div>
