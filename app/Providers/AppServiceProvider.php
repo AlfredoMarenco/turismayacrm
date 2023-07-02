69ea8c892546c520a84050f70fc46ce789b9ddde
Alfredo Gonzalez <alfredomarenco@boletea.com>
2023-07-01 18:37:03 -0600

    Cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setlocale('es');
         setlocale(LC_TIME,'es_ES');
         Carbon::setUTF8(true);
+        Schema::defaultStringLength(191);
     }
 }
